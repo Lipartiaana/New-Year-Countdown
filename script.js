57740df02721ebfeecf67ef69832627e20520e1f
Lipartiaana <124500573+Lipartiaana@users.noreply.github.com>
2024-01-16 17:11:12 +0400

    Updating the script to work for any year.

--- script.js
+++ script.js
@@ -1,38 +1,23 @@
-const newYearsDay = "1 Jan 2025";
-const currentDate = new Date();
-
 const days = document.getElementById("days");
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const number = document.querySelectorAll(".number");
 const heading = document.getElementById("heading");
+const countdownContainer = document.querySelector(".countdown-container");
 
 function countdown() {
-  const newYearsDate = new Date(newYearsDay);
   const currentDate = new Date();
-  const timeLeft = newYearsDate - currentDate;
 
-  const daysNumber = Math.floor(timeLeft / 1000 / 3600 / 24);
-  const hoursNumber = Math.floor((timeLeft / 1000 / 3600) % 24);
-  const minutesNumber = Math.floor((timeLeft / 1000 / 60) % 60);
-  const secondsNumber = Math.floor((timeLeft / 1000) % 60);
-
-  if (timeLeft <= 0 && timeLeft >= -1000 * 3600 * 24) {
-    heading.innerHTML = "Happy New Year!!";
-    for (let i = 0; i < number.length; i++) {
-      number[i].innerHTML = "";
-    }
-  } else if (timeLeft <= -1000 * 3600 * 24) {
-    heading.innerHTML = "Countdown Finished";
-    for (let i = 0; i < number.length; i++) {
-      number[i].innerHTML = "";
-    }
+  if (currentDate.getMonth() === 0 && currentDate.getDate() === 1) {
+    heading.innerHTML = `<h1 id="heading" style="color:red">
+        Happy New Year!
+        <span class="party-popper">🎉</span>
+      </h1>`;
+    countdownContainer.style.display = "none";
   } else {
-    days.innerHTML = formatTime(daysNumber);
-    hours.innerHTML = formatTime(hoursNumber);
-    minutes.innerHTML = formatTime(minutesNumber);
-    seconds.innerHTML = formatTime(secondsNumber);
+    showTime();
+    countdownContainer.style.display = "";
   }
 }
 
@@ -42,6 +27,24 @@ function formatTime(time) {
     return time;
   }
   return time;
+}
+
+function showTime() {
+  const currentDate = new Date();
+  const currentYear = new Date().getFullYear();
+  const newYearsDay = `1 Jan ${currentYear + 1} `;
+  const newYearsDate = new Date(newYearsDay);
+  const timeLeft = newYearsDate - currentDate;
+
+  const daysNumber = Math.floor(timeLeft / 1000 / 3600 / 24);
+  const hoursNumber = Math.floor((timeLeft / 1000 / 3600) % 24);
+  const minutesNumber = Math.floor((timeLeft / 1000 / 60) % 60);
+  const secondsNumber = Math.floor((timeLeft / 1000) % 60);
+
+  days.innerHTML = formatTime(daysNumber);
+  hours.innerHTML = formatTime(hoursNumber);
+  minutes.innerHTML = formatTime(minutesNumber);
+  seconds.innerHTML = formatTime(secondsNumber);
 }
 
 countdown();
